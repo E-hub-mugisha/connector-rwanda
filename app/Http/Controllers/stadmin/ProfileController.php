1f98499616d400ee16be1b1361e960b451e96176
mugisha erik <54761531+E-hub-mugisha@users.noreply.github.com>
2025-12-27 15:20:51 +0200

    email

--- app/Http/Controllers/stadmin/ProfileController.php
+++ app/Http/Controllers/stadmin/ProfileController.php
@@ -77,7 +77,7 @@ class ProfileController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function updateProfile(Request $request, $id)
     {
         $request->validate([
             'about' => 'required',
